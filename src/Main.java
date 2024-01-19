8079773425387d28905029a2c0fea5a6ed76314e
Lutti Davide <luttidavide004@gmail.com>
2024-01-19 11:57:48 +0100

    esercizio completo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student student = new Student("Mario", "Rossi", 1);
+
+        System.out.println("Name: " + student.getName());
+        System.out.println("Surname: " + student.getSurname());
+        System.out.println("Identification Number: " + student.getIdentificationNumber());
+
+        student.setName("Giovanni");
+        student.setSurname("Verdi");
+        student.setIdentificationNumber(56);
+
+        System.out.println("Updated name: " + student.getName());
+        System.out.println("Updated surname: " + student.getSurname());
+        System.out.println("Updated identification number: " + student.getIdentificationNumber());
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,45 @@
+public class Student {
+
+    private String name;
+    private String surname;
+    private int identificationNumber;
+
+    public Student(String name, String surname, int identificationNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.identificationNumber = identificationNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public void setIdentificationNumber(int identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", identificationNumber=" + identificationNumber +
+                '}';
+    }
+}
